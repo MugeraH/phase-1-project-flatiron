a36e8e36639fc66aa1535578de25d5e2e4d14ab8
Hughes Mugera <hughes.mugera@touchinspiration.com>
2022-05-22 14:20:05 +0300

    update functionality, add modal, add on change event

--- index.js
+++ index.js
@@ -1,25 +1,63 @@
-const api = "";
 const url = "https://api.coinstats.app/public/v1/coins?skip=0";
 let list = document.getElementById("list");
+let coinName = document.getElementById("modalCoinName");
+var coinData = {};
+let modal_coin = document.getElementById("modalCoin");
+let modalCoinLabel = document.getElementById("modalCoinLabel");
+let modalCoinPrice = document.getElementById("modalCoinPrice");
+let modalCoinImage = document.getElementById("coinImage");
+let coinSite = document.getElementById("coinWeb");
+let coinTwitter = document.getElementById("coinTwitter");
+var ebModal = document.getElementById("mySizeChartModal");
+var ebSpan = document.getElementsByClassName("ebcf_close")[0];
+let filterLimit = document.getElementById("selectLimit");
+let coinsData = [];
+let limit = 0;
 
 document.addEventListener("DOMContentLoaded", () => {
-  getCoins();
+  limit = filterLimit.options[filterLimit.selectedIndex].value;
+  getCoins(limit);
 });
 
-function getCoins() {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) =>
-     singleCoin(data.coins)
-    );
+ebSpan.onclick = function () {
+  ebModal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == ebModal) {
+    ebModal.style.display = "none";
+  }
+};
+
+function changeLimit() {
+  limit = filterLimit.options[filterLimit.selectedIndex].value;
+  getCoins(limit);
 }
 
-function singleCoin(coins) {
-  console.log(coins);
-  let data = ""
+function getCoins(limit) {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      coinsData = data.coins.map((coin) => {
+        coin["likes"] = 1;
 
-   coins.map((coin) => {
-     data += ` <div class="card-container">
+        return coin;
+      });
+
+      if (limit == "all") {
+        displayCoin(coinsData);
+      } else {
+        displayCoin(coinsData.slice(0, limit));
+      }
+    });
+}
+
+function displayCoin(coinsData) {
+  let data = "";
+
+  coinsData.map((coin) => {
+    // console.log(coin);
+    data += ` <div class="card-container">
             <div class="first-section">
               <div class="coin-details">
                 <h3>${coin.name}</h3>
@@ -37,18 +75,32 @@ function singleCoin(coins) {
               <!-- <span class="label">Price</span>
               <span class="value">$30,0000</span> -->
             </div>
-              <button class="btn">more</button>
-
+              <button class="btn" onclick="getCoinDetails('${
+                coin.id
+              }')">More info</button>
           </div>`;
-   });
-  
+  });
 
   list.innerHTML = data;
+}
+
+function getCoinDetails(coinId) {
+  let coinDetails = coinsData.filter((coin) => coin.id == coinId);
+
+  console.log(coinDetails[0]);
+
+  coinName.innerText = coinDetails[0].name;
+  modal_coin.innerText = coinDetails[0].name;
+  modalCoinLabel.innerText = coinDetails[0].symbol;
+  modalCoinPrice.innerText = convertPriceToKsh(coinDetails[0].price);
+  modalCoinImage.src = coinDetails[0].icon;
+  coinSite.href = coinDetails[0].websiteUrl;
+  coinTwitter.href = coinDetails[0].twitterUrl;
+
+  ebModal.style.display = "block";
 }
 
 function convertPriceToKsh(coinPrice) {
   let convertedPriceValue = coinPrice * 3469615.18;
   return convertedPriceValue.toFixed(1).replace(/\d(?=(\d{3})+\.)/g, "$&,");
 }
-
-// console.log(convertPriceToKsh(0.7172823631946439));
